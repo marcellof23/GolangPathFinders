2ccc2a5dc9c36d54fe4439bb52e90c06fd48ebc1
marcellof23 <01faria.marcello@gmail.com>
2021-04-05 01:36:41 +0700

    pull jesson

--- views/js/map.js
+++ views/js/map.js
@@ -1,53 +1,52 @@
-let markers=[];
+let markers = [];
 
 function initMap() {
+	const bandung = { lat: -6.9175, lng: 107.6191 };
 
-    const bandung = { lat: -6.9175, lng: 107.6191 };
+	window.map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 13,
+		center: bandung,
+	});
 
-    window.map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 13,
-        center: bandung,
-    });
-
-    window.map.addListener('click', (e)=>{
-        placeMarkerAndPanTo({lat: e.latLng.lat(),lng: e.latLng.lng()});
-    })
+	window.map.addListener("click", (e) => {
+		placeMarkerAndPanTo({ lat: e.latLng.lat(), lng: e.latLng.lng() });
+	});
 }
 
-const addMarker = (coords)=>{
-    var marker = new google.maps.Marker({
-        position: coords,
-        map:window.map,
-    })
-}
+const addMarker = (coords) => {
+	var marker = new google.maps.Marker({
+		position: coords,
+		map: window.map,
+	});
+};
 
 const placeMarkerAndPanTo = (coords) => {
-    let marker = new google.maps.Marker({
-      position: coords,
-      map: window.map,
-    });
-    marker.addListener('click', (e)=>{
-        console.log(marker.position);
-        console.log(marker.position.lat());
-        console.log(marker.position.lng());
-    })
-    marker.addListener('contextmenu', (e)=>{
-        e.preventDefault();
-        console.log("HAHAHAHAHAHA");
-        console.log(marker.position.lat());
-        console.log(marker.position.lng());
-    })
+	let marker = new google.maps.Marker({
+		position: coords,
+		map: window.map,
+	});
+	marker.addListener("click", (e) => {
+		console.log(marker.position);
+		console.log(marker.position.lat());
+		console.log(marker.position.lng());
+	});
+	marker.addListener("contextmenu", (e) => {
+		e.preventDefault();
+		console.log("HAHAHAHAHAHA");
+		console.log(marker.position.lat());
+		console.log(marker.position.lng());
+	});
 
-    markers.push(marker);
-    window.map.panTo(coords);
-}
+	markers.push(marker);
+	window.map.panTo(coords);
+};
 
-const clearMarker = ()=>{
-    for(let i=0;i<markers.length;i++){
-        markers[i].setMap(null);
-    }
-    markers = [];
-}
+const clearMarker = () => {
+	for (let i = 0; i < markers.length; i++) {
+		markers[i].setMap(null);
+	}
+	markers = [];
+};
 
 const clearMarkers = document.getElementById("clearNodes");
-clearMarkers.addEventListener('click',clearMarker);
+clearMarkers.addEventListener("click", clearMarker);
